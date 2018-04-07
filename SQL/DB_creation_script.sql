c9faef0ca03701adebca0fad86abfacde3c118e5
Constantina1996 <32490418+Constantina1996@users.noreply.github.com>
2018-04-07 13:48:39 +0300

    Add files via upload

--- SQL/DB_creation_script.sql
+++ SQL/DB_creation_script.sql
@@ -5,17 +5,37 @@ CREATE TABLE IF NOT EXISTS admins (
   adminID int(11) NOT NULL AUTO_INCREMENT,
   usernameAdmin varchar(255) NOT NULL,
   passwordAdmin varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  bidcount int(11) DEFAULT '0', 
   PRIMARY KEY (adminID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
+
+
+CREATE TABLE IF NOT EXISTS bids (
+  bidID int(11) NOT NULL AUTO_INCREMENT,
+  adminID int(11),
+  topic varchar(255) NOT NULL,
+  price float(11),
+  geography varchar(100),
+  PRIMARY KEY (bidID),
+  CONSTRAINT FOREIGN KEY (adminID) REFERENCES admins(adminID)
+    ON DELETE CASCADE
+	ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+
 CREATE TABLE IF NOT EXISTS users (
   userID int(11) NOT NULL AUTO_INCREMENT,
+  adminID int(11),
   username varchar(255) NOT NULL,
-  password varchar(255) NOT NULL,
   keywordsAboutInterests varchar(255),
   age int(11),
   sex varchar(1),
-  geo varchar(10),
-  PRIMARY KEY (userID)
+  geo varchar(100),
+  PRIMARY KEY (userID),
+  CONSTRAINT FOREIGN KEY (adminID) REFERENCES admins(adminID)
+    ON DELETE CASCADE
+	ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
